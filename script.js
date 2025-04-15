b03d3c8cee3f22d8160621fa30e58b2e6ce29515
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-16 02:55:59 +0400

    Quick Sort Done

--- script.js
+++ script.js
@@ -20,21 +20,44 @@
 
 // Selection Sort
 
-let arr = [5, 3, 6, 12, 32, 4, 23, 7];
+// let arr = [5, 3, 6, 12, 32, 4, 23, 7];
 
-function SelectionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let maxNumIndex = 0;
-    for (let j = 1; j < arr.length - i; j++) {
-      if (arr[j] > arr[maxNumIndex]) {
-        maxNumIndex = j;
-      }
+// function SelectionSort(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let maxNumIndex = 0;
+//     for (let j = 1; j < arr.length - i; j++) {
+//       if (arr[j] > arr[maxNumIndex]) {
+//         maxNumIndex = j;
+//       }
+//     }
+//     let temp = arr[maxNumIndex];
+//     arr[maxNumIndex] = arr[arr.length - i - 1];
+//     arr[arr.length - i - 1] = temp;
+//   }
+//   console.log(arr);
+// }
+
+// SelectionSort(arr);
+
+// Quick Sort
+
+let arr = [3, 6, 23, 9, 5, 12, 4];
+
+function QuickSort(arr) {
+  if (arr.length < 1) return arr;
+  let pivotIndex = Math.floor(arr.length / 2);
+  let pivot = arr[pivotIndex];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === pivotIndex) continue;
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
-    let temp = arr[maxNumIndex];
-    arr[maxNumIndex] = arr[arr.length - i - 1];
-    arr[arr.length - i - 1] = temp;
   }
-  console.log(arr);
+  return [...QuickSort(left), pivot, ...QuickSort(right)];
 }
 
-SelectionSort(arr);
+console.log(QuickSort(arr));
