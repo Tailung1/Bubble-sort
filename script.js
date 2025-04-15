acbbf84b746a6dc8912dcaf95b33a7e832750fc3
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-15 13:13:16 +0400

    Selection Sort DONE

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-let arr=[5,8,2,1,3,10,7,4]
+// // Bubble Sort
 
-for(let i=0;i<arr.length-1;i++) {
-    for(let j=0;j<arr.length-1-i;j++)  {
-        if(arr[j]>arr[j+1]) {
-            const temp=arr[j]
-            arr[j]=arr[j+1]
-            arr[j+1]=temp
-            // [arr[j],arr[j+1]]=[arr[j+1],arr[j]] // short version with destructuring
-        }
+// let arr=[5,8,2,1,3,10,7,4]
+
+// for(let i=0;i<arr.length-1;i++) {
+//     for(let j=0;j<arr.length-1-i;j++)  {
+//          console.log(i, j);
+//         if(arr[j]>arr[j+1]) {
+//             const temp=arr[j]
+//             arr[j]=arr[j+1]
+//             arr[j+1]=temp
+//             // [arr[j],arr[j+1]]=[arr[j+1],arr[j]] // short version with destructuring
+
+//         }
+//     }
+// }
+// console.log(arr)
+
+// Linear
+
+// Selection Sort
+
+let arr = [5, 3, 6, 12, 32, 4, 23, 7];
+
+function SelectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let maxNumIndex = 0;
+    for (let j = 1; j < arr.length - i; j++) {
+      if (arr[j] > arr[maxNumIndex]) {
+        maxNumIndex = j;
+      }
     }
+    let temp = arr[maxNumIndex];
+    arr[maxNumIndex] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+  console.log(arr);
 }
-console.log(arr)
+
+SelectionSort(arr);
